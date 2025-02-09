c81ec543da9826a8aa7eaa51044e55b6a0ced3b8
fafimba <fafimba@gmail.com>
2025-02-09 20:36:55 +0100

    statos

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/whow-damage-calculator/',
+  base: '/statos/',
   build: {
     outDir: 'dist'
   }
